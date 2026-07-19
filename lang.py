fa6ba89b6c6df2f5fa3a0c3df19e46a20555d04f
GSvegetable <3301296046@qq.com>
2026-07-19 12:58:02 +0800

    Update lang.py

--- lang.py
+++ lang.py
@@ -1,10 +1,6 @@
 UI_LANGUAGES = {
     'zh': {
-        'main_msg': "\n联系群主 @A_MM188\n加入频道 @dhhejenhhs\n\n",
-        'channel_msg': "请先加入频道体验完整功能", 
-        'welcome_btn': "欢迎语设置",
-        'timed_msg_btn': "群内定时消息",
-        'keyword_btn': "关键词屏蔽",
-        'captcha_btn': "进群人机验证"
+        'main_msg': "联系群主 @A_MM188\n\n加入频道 @dhhejenhhs",
+        'channel_msg': "请先加入频道体验完整功能"
     }
 }
